33e52a23ff96a5c358a323fabd44c037ca991ea6
shashank2707 <kingbreath2707@gmail.com>
2023-07-14 12:41:55 +0530

    unsubscruibe and bug fixes

--- consumer/src/main/java/com/wellwisher/consumer/service/ConsumeAndEmailService.java
+++ consumer/src/main/java/com/wellwisher/consumer/service/ConsumeAndEmailService.java
@@ -2,6 +2,12 @@ package com.wellwisher.consumer.service;
 
 import static com.wellwisher.consumer.constant.Constants.SOMETHING_WENT_WRONG;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
@@ -13,24 +19,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import com.rabbitmq.client.Channel;
 import com.wellwisher.consumer.exception.InternalServerErrorException;
 import com.wellwisher.consumer.pojo.BroadcastDTO;
 import com.wellwisher.consumer.pojo.People;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
 @Service
 public class ConsumeAndEmailService {
 
 	private static final Logger logger = LoggerFactory.getLogger(ConsumeAndEmailService.class);
 	@Autowired
-    private JavaMailSender mailSender;
+    JavaMailSender mailSender;
+	@Autowired
+	Configuration freemarkerConfig; 
 
     @RabbitListener(queues = "${spring.rabbitmq.queue}")
-    public void consumeAndSendEmail(People people, Channel channel) {
+    public void consumeAndSendEmail(People people, Channel channel) throws IOException, TemplateException {
     	logger.info("Inside consumeAndSendEmail()");
-        //sendEmail(people);
-        logger.info("Email sent successfully!!");
+        sendEmail(people);
+        logger.info("Inside consumeAndSendEmail, Email sent successfully!!");
     }
     
     @RabbitListener(queues = "${spring.rabbitmq.broadcastQueue}")
@@ -40,30 +53,35 @@ public class ConsumeAndEmailService {
         logger.info("Broadcast emailed sent successfully!!");
     }
 
-	private void sendEmail(People people) {
-        String emailContent = prepareEmailContent(people);
+	private void sendEmail(People people) throws IOException, TemplateException {
+		MimeMessage message = mailSender.createMimeMessage();
 		try {
-            MimeMessage message = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setTo(people.getEmail());
-            helper.setSubject("Happy " + people.getOccasion() + "!!");
-            helper.setText(emailContent);
-            mailSender.send(message);
+			
+	        MimeMessageHelper helper = new MimeMessageHelper(message);
+
+	        helper.setTo(people.getEmail());
+	        helper.setSubject("Happy " + people.getOccasion() + " !!");
+	        Map<String, Object> model = new HashMap<>();
+	        model.put("name", people.getName());
+	        model.put("occasion", people.getOccasion());
+	        helper.setText(geContentFromTemplate(model),true);
+	        mailSender.send(message);
+	        logger.info("Email sent successfully");;
+
         } catch (MessagingException e) {
             logger.error("Inside sendEmail, Exception occured | Reason : {}", e.getMessage());
             throw new InternalServerErrorException(SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
         }
 	}
-
-	private String prepareEmailContent(People people) {
-		String name = people.getName();
-	    
-	    StringBuilder sb  = new StringBuilder();
-	    sb.append("Dear " + name + ",\n\n");
-	    sb.append("Wishing you a very happy "+ people.getOccasion() + "! May this day bring joy, happiness, and success in your life.\n\n" );
-	    sb.append("Warm Regards,\n");
-	    sb.append("Well Wisher Team");
-
-	    return sb.toString();
-	}
+	
+	  public String geContentFromTemplate(Map < String, Object >model)     { 
+		  StringWriter stringWriter = new StringWriter();
+	        try {
+				freemarkerConfig.getTemplate("occasionTemplateForPerson.ftlh").process(model, stringWriter);
+			} catch (TemplateException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	        return stringWriter.getBuffer().toString();
+	  }
 }
