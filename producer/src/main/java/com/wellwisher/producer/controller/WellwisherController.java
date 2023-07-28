6efdb34a9b4bbc5e672a8bebdffbfc4c020d9865
Utkarsh Jaiswal <utalmighty@gmail.com>
2023-07-29 00:18:37 +0530

    Cross Origin

--- producer/src/main/java/com/wellwisher/producer/controller/WellwisherController.java
+++ producer/src/main/java/com/wellwisher/producer/controller/WellwisherController.java
@@ -2,6 +2,7 @@ package com.wellwisher.producer.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,13 +24,15 @@ public class WellwisherController{
 	public ResponseEntity<String> getStatus() {
 		return ResponseEntity.ok("Producer Operational");
 	}
-	
+
+	@CrossOrigin
 	@PostMapping(value = "/subscribe")
 	public ResponseEntity<String> subscribe(@RequestBody PeopleRequest people)
 	{
 		return wellwisherService.subscribe(people);
 	}
-	
+
+	@CrossOrigin
 	@GetMapping(value = "/unsubscribe")
 	public ResponseEntity<String> unsubscribe(@RequestParam(name="email") String email)
 	{
